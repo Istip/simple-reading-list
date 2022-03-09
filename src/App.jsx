973ada1336a950d6e2d23d1f17f20d77561e8f4c
Istip <isticsek@gmail.com>
2022-03-09 19:58:45 +0200

    fixed env stuff and created data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import { useState, useEffect } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from './utils/firebase';
+
 function App() {
+  const [books, setBooks] = useState();
+
+  useEffect(() => {
+    // Firestore collecton reference
+    const booksRef = collection(db, 'books');
+
+    // Function to fetch the data from the books referenec
+    getDocs(booksRef)
+      .then((snapshot) => {
+        // I have initialized an empty array
+        let fetchedData = [];
+
+        // Looping through the response data
+        snapshot.docs.forEach((book) => {
+          fetchedData.push({ ...book.data(), id: book.id });
+        });
+
+        // Finally added fetched data to the local state
+        setBooks(fetchedData);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  console.log(books);
+
   return (
     <div className="App">
-      <h1>{import.meta.env.VITE_TEST_TEXT}</h1>
+      <h1>Hello!</h1>
     </div>
   );
 }
---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+
+const config = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+};
+
+// initialize firebase app with configuration
+initializeApp(config);
+
+// initialize the services of firestore
+export const db = getFirestore();
